a78c108131c406b873d454ca83aa90336b82710f
Michael Anderson <ratboyjim@gmail.com>
2020-10-16 15:04:51 +0100

    Q3 done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,4 +4,9 @@ def get_pet_shop_name(dict):
 
 def get_total_cash(dict):
     return dict["admin"]["total_cash"]
-    
+
+def add_or_remove_cash(dict, amount):
+    if amount >= 0:
+        dict["admin"]["total_cash"] += amount
+    elif amount < 0:
+        dict["admin"]["total_cash"] -= amount  
