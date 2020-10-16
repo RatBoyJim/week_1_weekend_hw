ee82fed024a18be9fc5627c8b83a9a8c477d162f
Michael Anderson <ratboyjim@gmail.com>
2020-10-16 16:04:07 +0100

    Q11 done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -27,4 +27,7 @@ def get_pets_by_breed(pet_shop, breed):
             pets.append(pet["name"])
     return pets
 
- 
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
