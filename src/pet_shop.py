11ee814f28c92114f69e53ba733183d95e83448d
Michael Anderson <ratboyjim@gmail.com>
2020-10-17 14:42:34 +0100

    further 1 done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -57,3 +57,17 @@ def customer_can_afford_pet(customer, pet):
         return True
     else:
         return False
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if customer_can_afford_pet(customer, pet) == True:
+        add_pet_to_customer(customer, pet)
+        increase_pets_sold(pet_shop, 1)
+        remove_customer_cash(customer, pet["price"])
+        add_or_remove_cash(pet_shop, pet["price"])
+    # elif customer_can_afford_pet(customer, pet) == False:
+    #     return False
+
+        
+
+
+        
